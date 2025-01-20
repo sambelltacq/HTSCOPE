72df82468677afabcf80d3b9c1fed1bdaa35542f
Peter Milne <peter.milne@d-tacq.com>
2025-01-20 09:25:35 +0000

    $PFX: defaults to host:user: and zoom
     On branch main
    	modified:   db/htscope1.db
    	modified:   scripts/make_htscope_st.cmd.py
    	modified:   st.cmd

--- htscope1/scripts/make_htscope_st.cmd.py
+++ htscope1/scripts/make_htscope_st.cmd.py
@@ -2,6 +2,8 @@
 
 import argparse
 import sys
+import socket
+import os
 
 def print_preamble(args):
     if args.output == '-':
@@ -20,19 +22,20 @@ htscope1_registerRecordDeviceDriver(pdbbase)
 
 
 def print_uut(uut, args):
+    wsize=4 if args.data32 == 1 else 2
     args.fp.write(f"\n# uut {uut}\n")
     args.fp.write(f"""
-multiChannelScopeConfigure("{uut}", {args.nchan}, {args.ndata}, {4 if args.data32 == 1 else 2})
+multiChannelScopeConfigure("{uut}", {args.nchan}, {args.ndata}, {wsize})
     """)
     tm = "TIMEOUT=0"
     uutdb="../../db/htscope1.db"
     args.fp.write(f"""
-dbLoadRecords("{uutdb}","UUT={uut},{tm}")""")
+dbLoadRecords("{uutdb}","PFX={args.prefix},UUT={uut},{tm}")""")
     chdb = "../../db/htscope1_ch.db"
     for ix in range(args.nchan):
         ch = f"{ix+1:02}"
         args.fp.write(f"""
-dbLoadRecords("{chdb}","UUT={uut},CH={ch},IX={ix},{tm},NPOINTS={args.ndata}")""")
+dbLoadRecords("{chdb}","PFX={args.prefix},UUT={uut},CH={ch},IX={ix},{tm},NPOINTS={args.ndata}")""")
     args.fp.write(f"""
 asynSetTraceMask("{uut}",0,0xff))
     """)
@@ -56,12 +59,16 @@ def run_main(args):
         print_uut(uut, args)
     print_postamble(args)
 
+def default_prefix():
+    return f'{socket.gethostname()}:{os.environ.get("USER")}:'
+
 def get_parser():
     parser = argparse.ArgumentParser(description="create htscope epics record definition")
     parser.add_argument('--output', '-O', default="st.cmd", help="record definition file name [st.cmd]")
     parser.add_argument('--nchan', default=None, type=int, help="specify number of channels (or use hapi to automate)")
     parser.add_argument('--data32', default=None, type=int, help="set to 1 for d32 data (or use hapi to automate)")
     parser.add_argument('--ndata',  default=100000, type=int, help="number of data elements in WF")
+    parser.add_argument('--prefix', default=default_prefix(), type=str, help='prefix for PV\'s, default="$(hostname):$USER"')
     parser.add_argument('uuts', nargs='+', help="uut1[, uut2...]")
     return parser
 
